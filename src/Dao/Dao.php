ca3173bb160b4b16b9c56b32d5c1f68558fe42b7
Kim Eik <kim@heldig.org>
2014-10-01 12:50:17 +0200

    Cleaned up Dao class

--- src/Dao/Dao.php
+++ src/Dao/Dao.php
@@ -38,42 +38,99 @@ abstract class Dao {
     }
 
     /**
-     * Class name for entity this dao class handles.
-     * @return string
+     * Creates an entity in the database
+     * @param $obj
      */
-    public abstract function getEntityClass();
+    public function create($obj) {
 
-    /**
-     * @param $query
-     * @return bool|\mysqli_result
-     * @throws \Exception
-     */
-    protected function runQuery($query) {
-        $this->logger->info("Ran query: " . $query);
-        $result = $this->db->query($query);
-        if (!$result) {
-            throw new \RuntimeException("query failed: $query, error no: " . $this->db->errno . ", error:" . $this->db->error);
+        foreach ($this->tableData->getFieldsWithReference() as $referencedField) {
+            if ($this->tableData->hasPropertyValue($obj, $referencedField)) {
+                $referencedValue = $this->tableData->getPropertyValue($obj, $referencedField);
+                if (is_array($referencedValue)) {
+                    //collection of entities (one-to-many)
+                    foreach ($referencedValue as $entity) {
+                        $this->persistReferencedEntity($entity);
+                    }
+                } else {
+                    //single entity (one-to-one)
+                    $this->persistReferencedEntity($referencedValue);
+                }
+
+            }
         }
-        return $result;
+
+        $query = $this->tableData->getCreateSQL($obj);
+        $this->runQuery($query);
+
+        if ($this->tableData->getPrimaryKey()->isAutoIncrement()) {
+            $insertId = $this->db->insert_id;
+            $this->setIdValue($obj, $insertId);
+        }
+        $this->setVersion($obj);
     }
 
+    /**
+     * Retrieves an entity from the database
+     * @param $id
+     * @return null|object
+     * @throws \Exception
+     */
+    public function find($id) {
+        $entity = $this->findEntity($id);
+        $entity = $this->cast($entity);
+        return $entity;
+    }
+
+    /**
+     * Refreshes an entity from the database;
+     * @param $obj
+     */
     public function refresh(&$obj) {
         $entity = $this->findEntity($this->getIdValue($obj));
         $this->cast($entity, $obj);
     }
 
 
-    //FIXME this needs to be optimized!
+    /**
+     * Retrieves all entities from the database
+     * FIXME needs optimization
+     * @return array
+     */
+    public function all() {
+        $query = "SELECT id FROM {$this->tableData->getName()}";
+        $result = $this->runQuery($query);
 
-    private function findEntity($id) {
-        $result = $this->runQuery($this->tableData->getFindSQL($id));
-
-        if (!$result) {
-            return null;
+        $entities = array();
+        while ($row = $result->fetch_assoc()) {
+            $entities[] = $this->find($row['id']);
         }
 
-        $obj = $result->fetch_object();
-        return $obj;
+        return $entities;
+    }
+
+    /**
+     * Updates an entity to the database
+     * @param $obj
+     */
+    public function update($obj) {
+        if ($this->isTransient($obj)) {
+            throw new \InvalidArgumentException("Cannot update a transient entity");
+        }
+        $query = $this->tableData->getUpdateSQL($obj);
+        $this->runQuery($query);
+
+        if (!mysqli_affected_rows($this->db) > 0) {
+            throw new \RuntimeException("No rows updated by update query! either row has been deleted or another version was committed");
+        }
+    }
+
+    /**
+     * Determines if an entity is stored in the database
+     * @param $entity
+     * @return bool
+     */
+    public function isTransient($entity) {
+        return !property_exists($entity, $this->tableData->getVersionField()->getPropertyName());
     }
 
     /**
@@ -88,6 +145,40 @@ abstract class Dao {
         return $tableData->getPropertyValue($entity, $this->getIdPropertyName($tableData));
     }
 
+    private function setVersion($obj, $version = null) {
+        $versionField = $this->tableData->getVersionField();
+
+        if (is_null($version)) {
+            $version = $versionField->getDefault();
+        }
+
+        $obj->{$versionField->getPropertyName()} = $version;
+    }
+
+    /**
+     * @param $referencedValue
+     */
+    private function persistReferencedEntity($referencedValue) {
+        $dao = DaoFactory::getDaoFromEntity($referencedValue);
+        if (is_null($dao)) {
+            throw new \RuntimeException("Unhandled entity type: " . get_class($referencedValue));
+        }
+
+        $dao->create($referencedValue);
+    }
+
+    /**
+     * @param $entity
+     * @param $value
+     * @param null $tableData
+     */
+    private function setIdValue($entity, $value, $tableData = null) {
+        if (is_null($tableData)) {
+            $tableData = $this->tableData;
+        }
+        $tableData->setPropertyValue($entity, $this->getIdPropertyName($tableData), $value);
+    }
+
     /**
      * @param null $tableData
      * @return string
@@ -97,6 +188,32 @@ abstract class Dao {
             $tableData = $this->tableData;
         }
         return $tableData->getPrimaryKey()->getPropertyName();
+    }
+
+    /**
+     * @param $value
+     * @param $propertyName
+     * @return mixed|EntityProxy
+     */
+    private function getPropertyValue($value, $propertyName) {
+        $tableField = $this->tableData->getTableFieldByPropertyName($propertyName);
+        if (!is_null($value)) {
+            if ($tableField->isNumericType()) {
+                settype($value, "float");
+            } elseif ($tableField->isBoolType()) {
+                settype($value, "boolean");
+            } elseif ($tableField->isStringType()) {
+                settype($value, "string");
+            }
+
+            if ($tableField->isReference()) {
+                $referencedTableData = $tableField->getReference();
+                $entity = $referencedTableData->getEntityInstance();
+                $this->setIdValue($entity, $value, $referencedTableData);
+                $value = new EntityProxy($entity, DaoFactory::getDaoFromEntity($entity), $referencedTableData, false);
+            }
+        }
+        return $value;
     }
 
     /**
@@ -138,129 +255,37 @@ abstract class Dao {
     }
 
     /**
-     * @param $value
-     * @param $propertyName
-     * @return mixed|EntityProxy
-     */
-    private function getPropertyValue($value, $propertyName) {
-        $tableField = $this->tableData->getTableFieldByPropertyName($propertyName);
-        if (!is_null($value)) {
-            if ($tableField->isNumericType()) {
-                settype($value, "float");
-            } elseif ($tableField->isBoolType()) {
-                settype($value, "boolean");
-            } elseif ($tableField->isStringType()) {
-                settype($value, "string");
-            }
-
-            if ($tableField->isReference()) {
-                $referencedTableData = $tableField->getReference();
-                $entity = $referencedTableData->getEntityInstance();
-                $this->setIdValue($entity, $value, $referencedTableData);
-                $value = new EntityProxy($entity, DaoFactory::getDaoFromEntity($entity), $referencedTableData, false);
-            }
-        }
-        return $value;
-    }
-
-    /**
-     * @param $entity
-     * @param $value
-     * @param null $tableData
-     */
-    private function setIdValue($entity, $value, $tableData = null) {
-        if (is_null($tableData)) {
-            $tableData = $this->tableData;
-        }
-        $tableData->setPropertyValue($entity, $this->getIdPropertyName($tableData), $value);
-    }
-
-    public function all() {
-        $query = "SELECT id FROM {$this->tableData->getName()}";
-        $result = $this->runQuery($query);
-
-        $entities = array();
-        while ($row = $result->fetch_assoc()) {
-            $entities[] = $this->find($row['id']);
-        }
-
-        return $entities;
-    }
-
-    /**
      * @param $id
-     * @return null|object
+     * @return null|object|stdClass
+     */
+    private function findEntity($id) {
+        $result = $this->runQuery($this->tableData->getFindSQL($id));
+
+        if (!$result) {
+            return null;
+        }
+
+        $obj = $result->fetch_object();
+        return $obj;
+    }
+
+    /**
+     * @param $query
+     * @return bool|\mysqli_result
      * @throws \Exception
      */
-    public function find($id) {
-        $entity = $this->findEntity($id);
-        $entity = $this->cast($entity);
-        return $entity;
-    }
-
-    public function create($obj) {
-
-        foreach ($this->tableData->getFieldsWithReference() as $referencedField) {
-            if ($this->tableData->hasPropertyValue($obj, $referencedField)) {
-                $referencedValue = $this->tableData->getPropertyValue($obj, $referencedField);
-                if (is_array($referencedValue)) {
-                    //collection of entities (one-to-many)
-                    foreach ($referencedValue as $entity) {
-                        $this->persistReferencedEntity($entity);
-                    }
-                } else {
-                    //single entity (one-to-one)
-                    $this->persistReferencedEntity($referencedValue);
-                }
-
-            }
+    protected function runQuery($query) {
+        $this->logger->info("Ran query: " . $query);
+        $result = $this->db->query($query);
+        if (!$result) {
+            throw new \RuntimeException("query failed: $query, error no: " . $this->db->errno . ", error:" . $this->db->error);
         }
-
-        $query = $this->tableData->getCreateSQL($obj);
-        $this->runQuery($query);
-
-        if ($this->tableData->getPrimaryKey()->isAutoIncrement()) {
-            $insertId = $this->db->insert_id;
-            $this->setIdValue($obj, $insertId);
-        }
-        $this->setVersion($obj);
+        return $result;
     }
 
     /**
-     * @param $referencedValue
+     * Class name for entity this dao class handles.
+     * @return string
      */
-    private function persistReferencedEntity($referencedValue) {
-        $dao = DaoFactory::getDaoFromEntity($referencedValue);
-        if (is_null($dao)) {
-            throw new \RuntimeException("Unhandled entity type: " . get_class($referencedValue));
-        }
-
-        $dao->create($referencedValue);
-    }
-
-    private function setVersion($obj, $version = null) {
-        $versionField = $this->tableData->getVersionField();
-
-        if (is_null($version)) {
-            $version = $versionField->getDefault();
-        }
-
-        $obj->{$versionField->getPropertyName()} = $version;
-    }
-
-    public function update($obj) {
-        if ($this->isTransient($obj)) {
-            throw new \InvalidArgumentException("Cannot update a transient entity");
-        }
-        $query = $this->tableData->getUpdateSQL($obj);
-        $this->runQuery($query);
-
-        if (!mysqli_affected_rows($this->db) > 0) {
-            throw new \RuntimeException("No rows updated by update query! either row has been deleted or another version was committed");
-        }
-    }
-
-    public function isTransient($entity) {
-        return !property_exists($entity, $this->tableData->getVersionField()->getPropertyName());
-    }
+    public abstract function getEntityClass();
 }
